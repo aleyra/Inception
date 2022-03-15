424ffed0d6b981ccfdd123acb736ef061e692f64
lucille <lucille.burnet@gmail.com>
2022-03-15 18:21:43 +0100

    working on db

--- srcs/requirements/mariadb/files/mysql_install.sh
+++ srcs/requirements/mariadb/files/mysql_install.sh
@@ -1,15 +1,10 @@
-echo "pwd = ${MYSQL_ROOT_PASSWORD}"
 SECURE_MYSQL=$(expect -c "
 set timeout 10
 spawn mysql_secure_installation
 expect \"Enter current password for root (enter for none):\"
 send \"\r\"
 expect \"Change the root password?\"
-send \"y\r\"
-expect \"New password:\"
-send \"'${MYSQL_ROOT_PASSWORD}'\"
-expect \"Re-enter new password:\"
-send \"'${MYSQL_ROOT_PASSWORD}'\"
+send \"n\r\"
 expect \"Remove anonymous users?\"
 send \"y\r\"
 expect \"Disallow root login remotely?\"

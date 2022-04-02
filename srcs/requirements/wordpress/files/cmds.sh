1cc56efc7777b132a100087817fea59a39c241f2
lucille <lucille.burnet@gmail.com>
2022-04-02 17:20:42 +0200

    worphing on wp

--- srcs/requirements/wordpress/files/cmds.sh
+++ srcs/requirements/wordpress/files/cmds.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
-
-wp core install --allow-root --url=$DOMAIN_NAME --title="inception" --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PSW --admin_email=$ADMIN_MAIL --skip-email -path=/var/www/html/wordpress \
+su moi
+wp core install --allow-root --url=$DOMAIN_NAME --title="inception" --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PSW --admin_email=$ADMIN_MAIL --skip-email -path=/var/www/html/wordpress
+su root
+# expect "password:"
+# send "\r"

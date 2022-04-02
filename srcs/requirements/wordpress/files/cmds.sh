d4451f476c47fdbf3d530341c12230bcfb6350a1
lucille <lucille.burnet@gmail.com>
2022-04-02 16:58:25 +0200

    worphing on wp

---
+++ srcs/requirements/wordpress/files/cmds.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+wp core install --allow-root --url=$DOMAIN_NAME --title="inception" --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PSW --admin_email=$ADMIN_MAIL --skip-email -path=/var/www/html/wordpress \
--- srcs/requirements/wordpress/files/wp-config.php
+++ srcs/requirements/wordpress/files/wp-config.php
@@ -37,9 +37,9 @@ define( 'DB_CHARSET', 'utf8' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
-define('WP_CONTENT_DIR', '/var/www/html/wordpress/wp-content');
+// define('WP_CONTENT_DIR', '/var/www/html/wordpress/wp-content');
 
-define( 'UPLOADS', 'wp-content/uploads' );
+// define( 'UPLOADS', 'wp-content/uploads' );
 
 /**#@+
  * Authentication Unique Keys and Salts.

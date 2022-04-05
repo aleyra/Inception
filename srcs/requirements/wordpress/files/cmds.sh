b2cc33c2f19dd0671e05d224acfed408a82030f8
Lucille Burnet <lburnet@z1r6p3.42lyon.fr>
2022-04-05 17:30:26 +0200

    working on wp

--- srcs/requirements/wordpress/files/cmds.sh
+++ srcs/requirements/wordpress/files/cmds.sh
@@ -6,6 +6,7 @@ wp core config --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD -
 echo "config done ?"
 # wp core install --allow-root --url=$DOMAIN_NAME --title="inception" --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PSW --admin_email=$ADMIN_MAIL --skip-email -path=/var/www/html/wordpress
 wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PSW --admin_email=$ADMIN_MAIL
+wp user create $USER_NAME $USER_MAIL --role='editor' --user_pass=$USER_PSW
 su root
 # expect "password:"
 # send "\r"

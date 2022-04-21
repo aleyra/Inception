e3db3a059709bdf439e45f59cac9667ac789b35f
lucille <lucille.burnet@gmail.com>
2022-04-21 17:21:33 +0200

    worphing on wp

--- srcs/requirements/wordpress/files/cmds.sh
+++ srcs/requirements/wordpress/files/cmds.sh
@@ -5,5 +5,7 @@ echo "download done ?"
 wp core config --dbname=$DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$DB_HOST --dbprefix=wp_
 echo "config done ?"
 wp core install --url=$DOMAIN_NAME --title="Inception" --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PSW --admin_email=$ADMIN_MAIL
+echo "install done ?"
 wp user create $USER_NAME $USER_MAIL --role='editor' --user_pass=$USER_PSW
+echo "create done ?"
 su root

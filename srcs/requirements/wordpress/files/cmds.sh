c8f1c21cab2d5ad53a2dc4f1d46aa74ad80ab15e
lburnet <75073111+aleyra@users.noreply.github.com>
2022-05-24 17:43:11 +0200

    grr

--- srcs/requirements/wordpress/files/cmds.sh
+++ srcs/requirements/wordpress/files/cmds.sh
@@ -1,15 +1,10 @@
 #! /bin/bash
 
-# if [ ! -f "wp-config.php" ]; then
-	
-	echo "config done ?"
+# if ! wp --allow-root --path='/var/www/html/wordpress/' core is-installed;
+#then
 	wp core install --allow-root --url=$DOMAIN_NAME --title="Inception" --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PSW --admin_email=$ADMIN_MAIL 2>res2
-	# /var/www/html/wordpress# wp core install --allow-root --url=localhost/wordpress --title="Inception" --admin_user=nimda --admin_password=bouh --admin_email=nimda@mail.com
-	echo "install done ?"
+	# /var/www/html/wordpress# wp core install --allow-root --url='lburnet.42.fr' --title="Inception" --admin_user=nimda --admin_password=bouh --admin_email=nimda@mail.com
 	wp user create $USER_NAME $USER_MAIL --role='editor' --user_pass=$USER_PSW --allow-root 2>res3
-	echo "create done ?"
-	# service php7.3-fpm stop
-	# rm -rf cmds.sh
-	# php-fpm7.3 -F
+
 # fi
-# su root
+php-fpm7.3 -F

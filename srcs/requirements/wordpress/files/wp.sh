63bc9a0ae32a35a6ccd53b5a1219408148399ad8
lburnet <lburnet@42>
2022-06-02 16:15:17 +0200

    cleaning

--- srcs/requirements/wordpress/files/wp.sh
+++ srcs/requirements/wordpress/files/wp.sh
@@ -1,13 +1,6 @@
 #! /bin/bash
 
-# service php7.3-fpm start
-# service php7.3-fpm stop
-# if ! wp --allow-root --path='/var/www/html/wordpress/' core is-installed;
-#then
 
 	wp core install --allow-root --url=$DOMAIN_NAME --title=$DOMAIN_NAME --admin_user=$ADMIN_NAME --admin_password=$ADMIN_PSW --admin_email=$ADMIN_MAIL 2>res2
-	# /var/www/html/wordpress# wp core install --allow-root --url='lburnet.42.fr' --title="Inception" --admin_user=nimda --admin_password=bouh --admin_email=nimda@mail.com
 	wp user create $USER_NAME $USER_MAIL --role='editor' --user_pass=$USER_PSW --allow-root 2>res3
-# fi
 php-fpm7.3 -F
-# sleep infinity
